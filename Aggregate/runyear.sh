2d069043b1e0f576c3d79d859ff6ceef6f839219
Yue Ning <yuefuturening@gmail.com>
2014-02-25 15:20:31 -0500

    Update runyear.sh

--- Aggregate/runyear.sh
+++ Aggregate/runyear.sh
@@ -1,1 +1,8 @@
-hadoop jar /usr/share/hadoop/contrib/streaming/hadoop-streaming-1.2.1.jar -files stopwords.txt -input 'data/' -output 'test-year' -file SRC/YearMapper.py -file SRC/YearReducer.py -mapper 'YearMapper.py' -reducer 'YearReducer.py'
+hadoop jar /usr/share/hadoop/contrib/streaming/hadoop-streaming-1.2.1.jar\
+        -files stopwords.txt\
+        -input 'data/'\
+        -output 'test-year'\
+        -file SRC/YearMapper.py\
+        -file SRC/YearReducer.py\
+        -mapper 'YearMapper.py'\
+        -reducer 'YearReducer.py'
